cf1dc0fb6691ee3be0ff20f85edfd786d4eaba49
TheresaSchmotz <schmotz.theresa@gmail.com>
2018-11-22 11:58:45 +0100

    aufgabe 3 angefangen

--- Aufgabe2/src/Zahlenraten.java
+++ Aufgabe2/src/Zahlenraten.java
@@ -1,4 +1,4 @@
-import java.sql.SQLOutput;
+import javax.swing.*;
 import java.util.Random;
 
 public class Zahlenraten {
@@ -12,11 +12,21 @@ public class Zahlenraten {
         int zufallszahl = rnd.nextInt(max-min+1)+min;
         //java.util ist ein Package in das wir die Klasse Random importiert haben => java.util.Random
 
+        /**
         if (zufallszahl%2==0)
         {
             System.out.println("Da ist ne gerade Zahl drinnen.");
         }else{
             System.out.println("Da ist keine gerade Zahl drinnen.");
+         }*/
+
+        //Aufgabe 3
+        String ganzzahl = JOptionPane.showInputDialog("Bitte gib eine Ganzzahl zwischen 1 und 10 ein.");
+
+        if (ganzzahl == zufallszahl) {
+            System.out.println(true);
+        }else (ganzzahl != zufallszahl) {
+        System.out.println(false);
         }
     }
 }
